aa7993962b57460298ca701374af7d18e9ab4239
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-09-03 22:16:48 +0300

    Organize shell integrations

--- build.zig
+++ build.zig
@@ -25,8 +25,8 @@ pub fn build(b: *std.Build) void {
 
     // Runtime files belong to the Zig install graph so local installation,
     // cross-build prefixes, and release archives all contain the same tools.
-    b.installFile("tj.plugin.zsh", "share/tj/tj.plugin.zsh");
-    b.installFile("tj.plugin.fish", "share/tj/tj.plugin.fish");
+    b.installFile("shell/zsh/tj.plugin.zsh", "share/tj/tj.plugin.zsh");
+    b.installFile("shell/fish/tj.plugin.fish", "share/tj/tj.plugin.fish");
     b.installBinFile("contrib/tj-fence", "tj-fence");
     b.installBinFile("contrib/tj-grep", "tj-grep");
     b.installBinFile("contrib/tj-tape", "tj-tape");
@@ -104,8 +104,8 @@ pub fn build(b: *std.Build) void {
     integration_options.addOption([]const u8, "tjctl_exe", b.getInstallPath(.bin, "tjctl"));
     integration_options.addOption([]const u8, "version", manifest.version);
     integration_options.addOptionPath("selftest_exe", selftest.getEmittedBin());
-    integration_options.addOptionPath("plugin", b.path("tj.plugin.zsh"));
-    integration_options.addOptionPath("fish_plugin", b.path("tj.plugin.fish"));
+    integration_options.addOptionPath("plugin", b.path("shell/zsh/tj.plugin.zsh"));
+    integration_options.addOptionPath("fish_plugin", b.path("shell/fish/tj.plugin.fish"));
     integration_options.addOptionPath("bash_completion", completion_outputs[0]);
     integration_options.addOptionPath("zsh_completion", completion_outputs[1]);
     integration_options.addOptionPath("fish_completion", completion_outputs[2]);
